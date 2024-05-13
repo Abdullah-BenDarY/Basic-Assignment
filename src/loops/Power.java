28a04a23471996b949a3f3d7389dfd5e96091a82
AbdullahBendary <abdullahbendary7@gmail.com>
2024-05-13 21:49:24 +0300

    Finish power task

---
+++ src/loops/Power.java
@@ -0,0 +1,22 @@
+package loops;
+
+import java.util.Scanner;
+
+public class Power {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter base: ");
+
+        int base = scanner.nextInt();
+        System.out.print("Enter power: ");
+
+        int power = scanner.nextInt();
+        long result = 1;
+
+        for (int i = 1; i <= power; i++) {
+            result *= base;
+        }
+        System.out.println("The result is: " + result);
+    }
+}
