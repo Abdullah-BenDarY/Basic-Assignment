61465d105c60e64796a1ad9ad1f2f2c15f73d98b
AbdullahBendary <abdullahbendary7@gmail.com>
2024-05-17 17:29:09 +0300

    Finish Number Sequence task

---
+++ src/loops/NumberSequence.java
@@ -0,0 +1,15 @@
+package loops;
+
+public class NumberSequence {
+    public static void main(String[] args) {
+        int start = 7;
+        int increment = 7;
+        int count = 14;
+
+        for (int i = 0; i < count; i++) {
+            System.out.print(start + " ");
+            start += increment;
+        }
+    }
+}
+
