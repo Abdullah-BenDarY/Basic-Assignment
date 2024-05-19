8e1bea143b0865a63c017c98dfa6eae9b370e520
AbdullahBendary <abdullahbendary7@gmail.com>
2024-05-20 02:37:58 +0300

    Edit Count Num 2 task

--- src/functions_Arrays/CountNum2.java
+++ src/functions_Arrays/CountNum2.java
@@ -4,19 +4,28 @@ public class CountNum2 {
 
 
     public static void main(String[] args) {
-        int arr [] = {2,4,5,8,2,4,5,7,9,2,1,8,2,24,9,5,12,2,1};
-
-        int countNum = calculateCount(arr);
-
-        System.out.println("Number 2 has repeated in "+ countNum + " times.");
+        int arr[] = {2, 4, 5, 8, 2, 4, 5, 7, 9, 2, 1, 8, 2, 24, 9, 5, 12, 2, 1};
+        calculateCount(arr);
 
     }
-    public static int calculateCount(int arr[]){
-        int count =0;
-        for (int i=0 ; i < arr.length ; i++){
-            if (arr[i] ==2){
+
+    private static void printCount(int count) {
+
+        if (count > 0) {
+            System.out.println("Number 2 has repeated in " + count + " times.");
+        } else {
+            System.out.println("Not found.");
+
+        }
+    }
+
+    private static void calculateCount(int arr[]) {
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 2) {
                 count++;
             }
-        } return count;
+        }
+        printCount(count);
     }
 }
