e5cd7c4b25e1f9083b4f81797cb0f3b3b1148b76
AbdullahBendary <abdullahbendary7@gmail.com>
2024-06-06 06:12:13 +0300

    Finish oop first task test

--- src/oop/BankAccount.java
+++ src/oop/BankAccount.java
@@ -1,4 +1,41 @@
 package oop;
 
 public class BankAccount {
+    private String name;
+    private int accountNumber;
+    private int amount;
+    private String phone;
+
+    public BankAccount() {
+    }
+
+    public BankAccount(int amount) {
+        this.amount = amount;
+        accountNumber = 541541;
+        name = "Abdullah";
+        phone = "015444444444";
+
+    }
+
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
 }
+
--- src/oop/BankAccountTest.java
+++ src/oop/BankAccountTest.java
@@ -1,4 +1,12 @@
 package oop;
 
 public class BankAccountTest {
+    public static void main(String[] args) {
+        BankAccount bankAccount = new BankAccount(50000);
+
+        System.out.print("Client name: " + bankAccount.getName() +
+                "\nClient Phone: " + bankAccount.getPhone() +
+                "\nAccount number: " + bankAccount.getAccountNumber() +
+                "\nAvaiable amount: " + bankAccount.getAmount());
+    }
 }
