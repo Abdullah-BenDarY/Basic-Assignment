91cf793d02acaf1d82f22e8cb20eaa3a4d5e7c46
AbdullahBendary <abdullahbendary7@gmail.com>
2024-06-22 13:51:36 +0300

    finish shape task

--- src/oop/season2/season4Test/Main.java
+++
@@ -1,8 +0,0 @@
-package oop.season2.season4Test;
-
-public class Main {
-    public static void main(String[] args) {
-        Triangle triangle = new Triangle(4 , 4);
-        triangle.draw();
-    }
-}
